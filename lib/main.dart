652d2105562b36a40a97d7fe53a8b5e0edbc09b6
Prashant Jain <prashantjain25@gmail.com>
2025-06-21 11:59:52 +0530

    First Draft

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+ import 'package:flutter/material.dart';
+
+void main() =>  runApp( MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text('Paylent',
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+
+      ),
+    ),
+  ));
+
+
